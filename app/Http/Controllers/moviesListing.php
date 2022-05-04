7a0e5b2867c52e92de86815be0c533d9d6e8584e
ALESSANDROUVA <a.uva1988@gmail.com>
2022-05-04 17:45:10 +0200

    start css

--- app/Http/Controllers/moviesListing.php
+++ app/Http/Controllers/moviesListing.php
@@ -5,15 +5,16 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Movie;
 
-class moviesListing extends Controller
+class MoviesListing extends Controller
 {
     public function list() {
-        $myMovies = Movie::where('title')
-        ->orderBy('date' , 'asc')
+        $myMovies = Movie::orderBy('date','asc')
         ->get();
+        
         $data = [
         'movie' => $myMovies
         ];
+
     return view('moviesListing' , $data);
     }
 }
--- resources/views/moviesListing.blade.php
+++ resources/views/moviesListing.blade.php
@@ -1,8 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
 
-<ul>
-    @foreach ($movie as $movie)
-    
-    <li> {{ $movie->title }}</li>
-    @endforeach
-</ul>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
+<body>
+    <div class="container">
+
+        <h1>Film da vedere</h1>
+        
+        <ul>
+            @foreach ($movie as $movie)
+                <li>
+                    <span> {{ $movie->title }}</span>
+                    <span>Voto: {{ $movie->vote }}</span>
+                </li>
+            @endforeach
+        </ul>
+    </div>
+</body>
+</html>
 
